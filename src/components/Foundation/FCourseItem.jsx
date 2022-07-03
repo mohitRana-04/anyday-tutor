e97a117ae9e6206f478423205e1c203f09f60488
Mohit Rana <mohitrana04032003@gmail.com>
2022-07-03 19:15:03 +0530

    adding extra too much confused

--- src/components/Foundation/FCourseItem.jsx
+++ src/components/Foundation/FCourseItem.jsx
@@ -4,19 +4,30 @@ import classes from "./FcourseItem.module.css";
 function FCourseItem(props) {
   return (
     <div>
-      <ul className={classes.list}>
-        {props.fcourses.map((curElem) => (
-          <Foundation
-            key={curElem.id}
-            id={curElem.id}
-            img={curElem.img}
-            name={curElem.name}
-            Teacher={curElem.Teacher}
-            lang={curElem.lang}
-            price={curElem.price}
-          />
-        ))}
-      </ul>
+      {/* <br />
+      <br />
+      <br />
+      <br /> */}
+      <div className={classes.container}>
+        <h1>Prepare with Top Educator</h1>
+        <h4>
+          Access to India's best Foundation batch educators is just a
+          subscription away
+        </h4>
+        <ul className={classes.list}>
+          {props.fcourses.map((curElem) => (
+            <Foundation
+              key={curElem.id}
+              id={curElem.id}
+              img={curElem.img}
+              name={curElem.name}
+              Teacher={curElem.Teacher}
+              lang={curElem.lang}
+              price={curElem.price}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
